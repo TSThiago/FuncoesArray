f467f59bd8ea4570862dff75e3a72b69d877e617
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-13 20:36:29 -0300

    Criação da função que replica a função de array '.push'.

--- funcoes.js
+++ funcoes.js
@@ -1,43 +1,62 @@
-let array = []
+ let array = []
 
-// Função .push:
-// A função .push acrescenta uma "casa" no array e a preenche com a informação
-// que estiver entre parênteses
+// // Função .push:
+// // A função .push acrescenta uma "casa" no array e a preenche com a informação
+// // que estiver entre parênteses
 
-array.push(1)
+console.log(array.push(1))
 array.push(2)
 array.push(3)
 array.push(4)
 
 console.log(array)
 
-// Função .join([separator = '']):
-// A função .join faz uma concatenação com as informações (que se tornam strings) dentro do array. 
+// // Função .join([separator = '']):
+// // A função .join faz uma concatenação com as informações (que se tornam strings) dentro do array. 
 
-console.log(array.join(" - "))
+// console.log(array.join(" - "))
 
-// Função .concat()
-// A função .concat() cria um novo array e junta todas as informações 
-// passadas dentro do parâmetro.
+// // Função .concat()
+// // A função .concat() cria um novo array e junta todas as informações 
+// // passadas dentro do parâmetro.
 
-array2 = [2,3,4]
+// array2 = [2,3,4]
 
-arrayNovo = array.concat(array2)
-console.log(arrayNovo)
+// arrayNovo = array.concat(array2)
+// console.log(arrayNovo)
 
-// Função .filter
-// A função .filter cria um array novo de acordo com a função
-// passada dentro do parâmetro.
+// // Função .filter
+// // A função .filter cria um array novo de acordo com a função
+// // passada dentro do parâmetro.
 
-function EhMaior(valor){
-    return valor > 1
+// function EhMaior(valor){
+//     return valor > 1
+// }
+
+// arrayNovo2 = arrayNovo.filter(EhMaior)
+// console.log(arrayNovo2)
+
+
+// // Função .slice
+// // A função .slice serve para retornar uma parte de um array.
+
+// console.log(parteArray = array.slice(1,3))
+
+// Parte 2 - Replicando as funções
+
+let arrayTeste = []
+
+console.log(ReplicandoPush(2))
+console.log(arrayTeste)
+// Função .push
+
+function ReplicandoPush(parametro){
+    arrayTeste[arrayTeste.length] = parametro 
+    return parametro
 }
 
-arrayNovo2 = arrayNovo.filter(EhMaior)
-console.log(arrayNovo2)
+// Função .pop
 
+// Função idexOf
 
-// Função .slice
-// A função .slice serve para retornar uma parte de um array.
-
-console.log(parteArray = array.slice(1,3))
+// Função includes
